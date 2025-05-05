0e004746a45402d13e4be0d33312f80dad25384d
Heloiisee <Heloiseguicheron71@gmail.com>
2025-05-05 15:05:34 -0400

    Modification du vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/styles.scss', 
-                'resources/js/app.js'
-            ],
-            refresh: true,
-        }),
-    ],
+  base: '/build/', // <--- essentiel
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/sass/styles.scss'],
+      refresh: true,
+    }),
+  ],
 });
