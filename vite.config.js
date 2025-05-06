98e11cddea0d173b46d08bb3b21343427685a991
Heloiisee <Heloiseguicheron71@gmail.com>
2025-05-05 22:05:19 -0400

    Changement de app.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   base: '/build/', // <--- essentiel
   plugins: [
     laravel({
-      input: ['resources/js/app.js', 'resources/sass/styles.scss'],
+      input: [
+        'resources/js/app.js', 
+        'resources/sass/styles.scss',
+        'resources/css/app.css',
+        'resources/js/intersectionObserver.js',
+        'resources/js/script.js',],
       refresh: true,
     }),
   ],
